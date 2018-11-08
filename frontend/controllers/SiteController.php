b58b357ffea00ef3ed499eea2c0de6d7f93af8c6
bill <skuangd@gmail.com>
2018-11-08 22:01:18 +0800

    =

--- frontend/controllers/SiteController.php
+++ frontend/controllers/SiteController.php
@@ -72,6 +72,7 @@ class SiteController extends Controller
      */
     public function actionIndex()
     {
+        // file_put_contents('d:/1.txt', var_export(Yii::$app->session, 1) . Yii::$app->session->get('username'));
         return $this->render('index');
     }
 
@@ -88,6 +89,7 @@ class SiteController extends Controller
 
         $model = new LoginForm();
         if ($model->load(Yii::$app->request->post()) && $model->login()) {
+            Yii::$app->session->set('username', $model->username);
             return $this->goBack();
         } else {
             $model->password = '';
@@ -218,8 +220,8 @@ class SiteController extends Controller
         // String
         $redis = Yii::$app->redis;
         echo $redis->get('name');
-        // Hash、List、Set、SortedSet、HyperLogLog、GEO、Pub/Sub、Transaction、Script、Connection、Server
-        $redis->zadd('names', 0, 'bill'); // 增
+        // SortedSet、HyperLogLog、GEO、Pub/Sub、Transaction、Script、Connection、Server
+        /*$redis->zadd('names', 0, 'bill'); // 增
         $redis->zadd('names', 4, 'bill'); // 改
         $redis->zadd('names', 1, 'ivy', 2, 'admin', 3, json_encode(['sa', 'root'])); // 增
         $redis->zrem('names', 'bill66'); // 删
@@ -227,6 +229,38 @@ class SiteController extends Controller
         echo '<pre>';
         // var_dump($redis->zrange('names', 0, 2, 'withscores')); // WITHSCORES
         var_dump($redis->zrange('names', 0, -1, 'withscores')); // 查
+        echo '</pre>';*/
+
+        // Set
+        /*$redis->sadd('addr', 'guangzhou', 'manila');
+        $redis->sadd('addr', 'beijing');
+        $this->_debug($redis->smembers('addr'));*/
+
+        // Hash
+        /*$redis->hset('language', 'programer1', 'php');
+        $redis->hset('language', 'programer2', 'js');
+        $redis->hset('language', 'programer3', 'html');
+        $redis->hset('language', 4, 'go');
+        $redis->hset('language', 5, 'python');
+        $this->_debug($redis->hget('language', 4));
+        $this->_debug($redis->hvals('language'));
+        $this->_debug($redis->hkeys('language'));
+        $redis->hmset('db', 1, 'mysql', 2, 'mongodb', 3, 'redis', 4, 'oracle', 5, 'sqlserver', 6, json_encode(['aa', 'bb', 'cc']));
+        $redis->hdel('db', 6);
+        $this->_debug($redis->hget('db', 6));
+        $this->_debug($redis->hmget('db', 1, 2, 3));*/
+
+        // List
+        /*$redis->lpush('runoob', 'redis');
+        $redis->rpush('runoob', 'mongodb');
+        $redis->lpush('runoob', 'mysql');
+        $this->_debug($redis->lrange('runoob', 0, -1));*/
+    }
+
+    private function _debug($a)
+    {
+        echo '<pre>';
+        var_dump($a);
         echo '</pre>';
     }
 }
