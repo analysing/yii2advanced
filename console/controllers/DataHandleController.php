d128edd28d766501a19a8cdeaa80ad2c1e80a74b
bill <skuangd@gmail.com>
2018-11-18 01:29:27 +0800

    =

---
+++ console/controllers/DataHandleController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace console\controllers;
+
+/**
+ * 数据处理
+ */
+class DataHandleController extends \yii\console\Controller
+{
+    // .\yii data-handle/index
+    public function actionIndex()
+    {
+        echo 'hahaha..';
+        return 0;
+    }
+
+    
+}
